abf95c6d32f07af0816f49bff29013f8e980dc77
SouErick <ericksousasaraiva@gmail.com>
2025-08-05 16:49:56 -0300

    Inicializando projeto spring boot

---
+++ src/main/java/com/example/demo/IdoeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdoeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdoeApiApplication.class, args);
+	}
+
+}
